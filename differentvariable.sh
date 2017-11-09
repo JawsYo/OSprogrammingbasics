4593bb6da1d40fd1c001cef3d1bfb5711c47c64b
Rieple <irieple196@studentubuntu14.ecc.iwcc.edu>
2017-11-09 16:16:53 -0600

    First bash scripts 'n variables

---
+++ differentvariable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+name="Ian Rieple"
+echo "Hello, $name"
+echo " "
+read -p "How old are you?" age
+echo "$name is $age years old and you are $1." 
+echo " "
+read -p "What is your favorite color" color
+echo "$color is good too but my favorite is purple"  
---
+++ variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+name="Ian Rieple"
+echo "Hello, $name"
+echo " "
+read -p "How old are you?" age
+echo "$name is $age years old." 
+echo " "
