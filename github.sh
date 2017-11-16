3af0798693cea9b3362c3d9e0c789fa8ba76357a
Rieple <irieple196@studentubuntu14.ecc.iwcc.edu>
2017-11-16 15:43:41 -0600

    Changed_github_script

--- github.sh
+++ github.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-read -p "What would you like to name this change?" note 
+read -p "What would you like to name this change? " note 
+cd ~/Projects/OSprogrammingbasics
 git add .
 git commit -m $note
 git push -u origin master 
+cd -
