e75d702d5c3e3598300b44f1ff6a2588585fcb80
Rieple <irieple196@studentubuntu14.ecc.iwcc.edu>
2017-11-16 15:21:40 -0600

    More scripts

--- if-then.sh
+++ if-then.sh
@@ -2,7 +2,10 @@
 
 if [ $1 -lt 10 ] 
 then 
-	echo "Your number is less than 10."
+	echo "$1 is less than 10."
+elif [ $1 -eq 10 ] 
+then 
+	echo "Your number is equal to 10."
 else
 	echo "Your number is greater than 10."
 fi
---
+++ loop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for car in ford toyota othercar mazda subaru  
+do
+	echo "$car"
+	sleep 1
+done
